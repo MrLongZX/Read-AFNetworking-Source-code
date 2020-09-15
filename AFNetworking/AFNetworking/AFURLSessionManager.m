2aa223fa17fee0829914d854b5d7185636fa0903
icitic_suyoulong <icitic_suyoulong@csmail.citicbank.com>
2020-09-15 23:50:47 +0800

    no message

--- AFNetworking/AFNetworking/AFURLSessionManager.m
+++ AFNetworking/AFNetworking/AFURLSessionManager.m
@@ -1166,14 +1166,18 @@ didReceiveChallenge:(NSURLAuthenticationChallenge *)challenge
     NSAssert(self.sessionDidReceiveAuthenticationChallenge != nil, @"`respondsToSelector:` implementation forces `URLSession:didReceiveChallenge:completionHandler:` to be called only if `self.sessionDidReceiveAuthenticationChallenge` is not nil");
 
     NSURLCredential *credential = nil;
-        /* Use the specified credential, which may be nil */
-        NSURLSessionAuthChallengeUseCredential = 0,
-        /* Default handling for the challenge - as if this delegate were not implemented; the credential parameter is ignored. */
-        NSURLSessionAuthChallengePerformDefaultHandling = 1,
-        /* The entire request will be canceled; the credential parameter is ignored. */
-        NSURLSessionAuthChallengeCancelAuthenticationChallenge = 2,
-        /* This challenge is rejected and the next authentication protection space should be tried; the credential parameter is ignored. */
-        NSURLSessionAuthChallengeRejectProtectionSpace = 3,
+
+    // 使用指定证书
+    // NSURLSessionAuthChallengeUseCredential = 0,
+    /* Default handling for the challenge - as if this delegate were not implemented; the credential parameter is ignored. */
+    // 默认的处理方式,忽略证书
+    // NSURLSessionAuthChallengePerformDefaultHandling = 1,
+    /* The entire request will be canceled; the credential parameter is ignored. */
+    // 忽略书证, 并取消这次请求
+    // NSURLSessionAuthChallengeCancelAuthenticationChallenge = 2,
+    /* This challenge is rejected and the next authentication protection space should be tried; the credential parameter is ignored. */
+    // 拒绝当前这一次, 下一次再询问
+    // NSURLSessionAuthChallengeRejectProtectionSpace = 3,
     
     NSURLSessionAuthChallengeDisposition disposition = self.sessionDidReceiveAuthenticationChallenge(session, challenge, &credential);
 
